c42265d871436fe25ad09d9828dca262cecb985a
YOGESHnp <yogesnp.tc@gmail.com>
2025-09-11 17:02:41 +0530

    Add eslint-plugin-jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,20 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    'jest/globals': true, // <-- add this to recognize Jest globals
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'coverage/'], // <-- ignore coverage
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jest'], // <-- add jest plugin
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
@@ -18,4 +22,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
