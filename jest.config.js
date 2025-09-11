3e7f2cb3dd228381f65872495b554f7a9d0d3d77
YOGESHnp <yogesnp.tc@gmail.com>
2025-09-11 16:52:57 +0530

    Add jest-dom, react dependencies

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest'
-  },
-  moduleFileExtensions: ['js', 'jsx']
-};
